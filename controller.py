b7e79a28fa4fb8e8a8b39394da4ed73a5229b6a9
Harish Gokul G <mtpharishgokul@gmail.com>
2024-05-23 21:14:01 +0530

    Update controller.py

--- controller.py
+++ controller.py
@@ -4,7 +4,7 @@ from validations import validateRequestBody
 def sendChatRequest(payload):
     
     if(  (type(payload) is not dict) or ("appointment_date" not in payload) or ("appointment_time" not in payload)):
-        rise Exception(str(payload))
+        raise Exception(str(payload))
         return {"message": "check the paylaod"},400
     print(payload)
     return {"message": "Collected appointment details","data": payload},200
