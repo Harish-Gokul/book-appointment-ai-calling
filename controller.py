67bc0e777fbf1ab5f4c10ce1edc792363d5b0410
Harish Gokul G <mtpharishgokul@gmail.com>
2024-05-23 18:34:21 +0530

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,9 +1,9 @@
 from flask import request
 from validations import validateRequestBody
-import json
+
 def sendChatRequest(payload):
     
-    if(  (type(json.loads(payload)) is not dict) or ("appointment_date" not in payload) or ("appointment_time" not in payload)):
+    if(  (type(payload) is not dict) or ("appointment_date" not in payload) or ("appointment_time" not in payload)):
         return {"message": "check the paylaod"},400
     print(payload)
     return {"message": "Collected appointment details","data": payload},200
