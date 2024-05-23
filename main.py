ae6c0df1e54f34ed00f1acf16f4fd34b2eb7bb32
Harish Gokul G <mtpharishgokul@gmail.com>
2024-05-23 15:13:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,13 @@ from decouple import config
 app = Flask(__name__)
 CORS(app)
 
-API_SECRET_KEY = config("webhook_api_secret")
-def verify_webhook(data, hmac_header):
-	 
-    digest = hmac.new(API_SECRET_KEY.encode('utf-8'), data, digestmod=hashlib.sha256).digest()
-    computed_hmac = base64.b64encode(digest)
-
-    return hmac.compare_digest(computed_hmac, hmac_header.encode('utf-8'))
+ 
     
 # PC_CB_8 - PC_CB_11 - to handle request with /chatbot as endpoint
 @app.route("/bookappointment",methods=["POST"])
 def handleChatRequest():
     payload = request.get_json()
-    print(payload)
-    verified = verify_webhook(payload, request.headers.get('X-Signature-SHA256'))
-    if not verified:
-        abort(401)
+    raise TypeError(str(payload))
     return sendChatRequest(payload)
 
 # PC_CB_6 to PC_CB_7 -  call app.run only if this file is executed
