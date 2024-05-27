8654e91d2a81344372cd3b242f351c8018fc767d
Harish Gokul G <mtpharishgokul@gmail.com>
2024-05-27 13:47:41 +0530

    Update controller.py

--- controller.py
+++ controller.py
@@ -10,3 +10,9 @@ def sendChatRequest(payload):
     return {"message": "Collected appointment details","data": payload},200
      
     
+def reconnectLater(payload):
+    if(  (type(payload) is not dict) or ("appointment_date" not in payload) or ("appointment_time" not in payload)):
+        raise Exception(str(payload))
+        return {"message": "check the paylaod"},400
+    print(payload)
+    return {"message": "Collected appointment details","data": payload},200
